5b62050ddacaddfd554a4232bfdc275d219c151b
haenin <heyone0022@gamail.com>
2025-08-03 15:32:53 +0900

    mentoringgoodsreport
    insert

---
+++ mentoringgoodsreport/mentoringgoodsreport_insert.sql
@@ -0,0 +1,17 @@
+-- 매칭 5번(멘티22)에 대한 신고들
+INSERT INTO mentoringgoodsreport (mentoringgoodsreport_detail, matching_number) 
+VALUES ('멘토가 수업에 늦습니다', 5);
+
+INSERT INTO mentoringgoodsreport (mentoringgoodsreport_detail, matching_number) 
+VALUES ('멘토링 품질이 떨어집니다', 5);
+
+INSERT INTO mentoringgoodsreport (mentoringgoodsreport_detail, matching_number) 
+VALUES ('약속된 커리큘럼과 다릅니다', 5);
+
+INSERT INTO mentoringgoodsreport (mentoringgoodsreport_detail, matching_number) 
+VALUES ('멘토의 태도가 불친절합니다', 5);
+
+INSERT INTO mentoringgoodsreport (mentoringgoodsreport_detail, matching_number) 
+VALUES ('멘토링 내용이 허위입니', 5);
+
+select * from mentoringgoodsreport;
